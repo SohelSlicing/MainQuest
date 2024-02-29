f99ecaef006b8370fd49ab4a35a6c3ed35ae64d7
SohelSlicing <sohelshaikh23.02@gmail.com>
2024-02-29 09:46:49 +0530

    Prompt testing

--- frontend/main.py
+++ frontend/main.py
@@ -1,37 +1,13 @@
 import streamlit as st
-
-from pages import flashcard
-
-class MultiApp:
-    def __init__(self):
-        self.apps = []
-
-    def add_app(self, title, func):
-        """Adds a new application.
-        Parameters
-        ----------
-        func:
-            the python function to render this app.
-        title:
-            title of the app. Appears in the dropdown in the sidebar.
-        """
-        self.apps.append({
-            "title": title,
-            "function": func
-        })
-
-    def run(self):
-        # app = st.sidebar.radio(
-        app = st.selectbox(
-            'Navigation',
-            self.apps,
-            format_func=lambda app: app['title'])
-
-        app['function']()
+import requests
 
 st.set_page_config(page_title="MainQuest", page_icon="frontend/assets/logo.jpg")
 
 # Header with logo
-col1, col2 = st.columns([1, 6])
-col1.image("frontend/assets/logo.jpg", width=100)
-col2.header("MainQuest")
+st.header("MainQuest")
+
+
+
+if st.button("Send get request"):
+    res = requests.get(url = url)
+    st.write(res.text)
--- frontend/pages/flashcard.py
+++ frontend/pages/flashcard.py
@@ -1,3 +1,31 @@
 import streamlit as st
+from PyPDF2 import PdfReader
 
-st.write("flashcards!")
+st.set_page_config(page_title="Flashcards")
+st.header("Active Recall")
+
+def get_pdf_text(pdf_docs):
+    text=""
+    for pdf in pdf_docs:
+        pdf_reader= PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text+= page.extract_text()
+    return  text
+
+def main():
+    source_material = st.file_uploader("Upload your PDF Files and Click on the Submit & Process Button", accept_multiple_files=True)
+    if st.button("Submit & Process"):
+        with st.spinner("Processing..."):
+            try:
+                raw_text = get_pdf_text(source_material)
+                st.success("PDF Processed Successfully")
+            except:
+                st.error("Processing failed")
+
+        no_of_questions = st.number_input("no of questions expected")
+
+        if st.button("Generate Flash cards"):
+            pass
+
+if __name__ == "__main__":
+    main()
